68a7a07e9c7666e2cd344097f36231bbea3607bc
polashds <polashtechai@gmail.com>
2025-05-11 21:16:00 +0600

    BMI calculator added

---
+++ Py_100_Project/research/calculator_bmi2.py
@@ -0,0 +1,7 @@
+def bmi_calculator():
+    weight = float(input("Enter your weight (kg): "))
+    height = float(input("Enter your height (m): "))
+    bmi = weight / (height ** 2)
+    print(f"Your BMI is: {bmi:.2f}")
+
+bmi_calculator()
